c132f67e6a593211e9e15d9715760d90bdad6f50
lzxm160 <lzxm160@sina.com>
2016-04-13 17:09:13 +0800

    lib.a

--- source/corvusoft/restbed/session.cpp
+++ source/corvusoft/restbed/session.cpp
@@ -112,14 +112,14 @@ namespace restbed
     
     void Session::close( const Response& response )
     {
-        const std::string temp_content( response.get_body().begin( ), response.get_body().end( ) );
-        std::cout<<temp_content<<":"<<__FILE__<<":"<<__LINE__<<std::endl;
-        
-        
         auto session = shared_from_this( );
         
         m_pimpl->transmit( response, [ this, session ]( const boost::system::error_code & error, size_t )
         {
+            const std::string temp_content( response.get_body().begin( ), response.get_body().end( ) );
+            std::cout<<temp_content<<":"<<__FILE__<<":"<<__LINE__<<std::endl;
+        
+        
             if ( error )
             {
                 const auto message = String::format( "Close failed: %s", error.message( ).data( ) );
